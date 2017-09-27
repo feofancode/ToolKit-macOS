9633feb289edf45becfbfd3ccc9b0f6c96056c2c
fengfanhua <fengfanhua@meituan.com>
2017-09-27 17:15:37 +0800

    update url encode

--- ToolKit/Encode/EncodeViewController.swift
+++ ToolKit/Encode/EncodeViewController.swift
@@ -10,9 +10,19 @@ import Cocoa
 
 class EncodeViewController: NSViewController {
 
+    @IBOutlet var textView: NSTextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        textView.font = NSFont(name: "Menlo", size: 16)
     }
     
+    @IBAction func encodeButtonClicked(_ sender: Any) {
+        let characterSet = CharacterSet(charactersIn: "!*'();:@&=+$,/?%#[]")
+        textView.string = textView.string?.addingPercentEncoding(withAllowedCharacters: characterSet.inverted)
+    }
+    @IBAction func decodeButtonClicked(_ sender: Any) {
+        textView.string = textView.string?.removingPercentEncoding
+    }
 }
